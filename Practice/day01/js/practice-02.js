e49878c6ac3cb2599299898a5b631e0406a386f8
Yigit Er <mr.yigiter222@gmail.com>
2023-06-18 08:11:15 +0300

    Practice day 01

---
+++ Practice/day01/js/practice-02.js
@@ -0,0 +1,10 @@
+
+// Kullanıcıdan dikdörtgenin uzunluğunu ve genişliğini alalım
+let dikdortgenUzunlugu = parseInt(prompt("Dikdörtgenin uzunluğunu girin:"));
+let dikdortgenGenisligi = parseInt(prompt("Dikdörtgenin genişliğini girin:"));
+ // Alanı hesaplayalım ve sonucu bir değişkene atayalım.
+let dikdortgenAlani = dikdortgenUzunlugu * dikdortgenGenisligi;
+ // Sonucu ekrana yazdıralım.
+console.log("Dikdörtgenin alanı: " + dikdortgenAlani);
+
+console.log(`Dikdörtgenin alanı: ${dikdortgenAlani}`);
